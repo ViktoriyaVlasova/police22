0d1edfa9582f183ec5d0fa8bd34165b31da3f9bc
ViktoriyaVlasova <71847067+ViktoriyaVlasova@users.noreply.github.com>
2022-09-21 14:05:56 +0300

    Add files via upload

---
+++ police vlasova/police vlasova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  police vlasova
+//
+//  Created by user on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
